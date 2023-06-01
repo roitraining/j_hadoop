d554b01e4e762d0200d487ab11b60d2ccbbe1bd5
joegagliardo <joey@me.com>
2023-06-01 14:02:36 -0400

    4.1.0

---
+++ shipper_to_json.py
@@ -0,0 +1,22 @@
+# run with spark-submit shipper_to_json.py
+# then cat /tmp/shippersjson/*
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql.types import *
+
+sc = SparkContext()
+spark = (SparkSession.builder.appName('notebook') 
+    .setMaster('local')
+    .enableHiveSupport().getOrCreate()
+    )
+
+shipperSchema = StructType([
+StructField('shipperid', StringType()), 
+StructField('shippername', StringType()),
+StructField('phone', StringType())
+])
+shippers = spark.read.csv('file:///class/datasets/northwind/TSV/shippers', 
+sep='\t', header=False, inferSchema=False, schema=shipperSchema)
+shippers.write.mode('overwrite').json('file:///tmp/shippersjson')
+
+
